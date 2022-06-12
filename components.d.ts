aeab047cdd7f1552f525837bfb472ba1b560e262
guxuerui <im.guxuerui@gmail.com>
2022-06-12 23:11:06 +0800

    拆分模板组件和方法

--- components.d.ts
+++ components.d.ts
@@ -5,8 +5,10 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Block: typeof import('./src/components/Block.vue')['default']
     Counter: typeof import('./src/components/Counter.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
+    MineBlock: typeof import('./src/components/MineBlock.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import type { BlockState } from '~/types'
+import { isDev, toggleDev } from '~/composables'
 
 const WIDTH = 10
 const HEIGHT = 10
-const state = ref(
-  Array.from({ length: HEIGHT }, (_, y) =>
+const state = ref<BlockState[][]>([])
+
+const reset = () => {
+  state.value = Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH },
       (_, x): BlockState => ({
         x,
@@ -13,8 +16,8 @@ const state = ref(
         reveoled: false,
       }),
     ),
-  ),
-)
+  )
+}
 
 const directions = [
   [-1, -1],
@@ -39,8 +42,8 @@ const getSiblings = (block: BlockState) => {
 }
 
 // 算下每个格子周围的炸弹数
-const updateNumbers = () => {
-  state.value.forEach((row, y) => {
+const updateNumbers = (state: BlockState[][]) => {
+  state.forEach((row, y) => {
     row.forEach((block, x) => {
       if (block.mine)
         return
@@ -54,8 +57,8 @@ const updateNumbers = () => {
 }
 
 // 生成炸弹
-const generateMines = (initial: BlockState) => {
-  for (const row of state.value) {
+const generateMines = (state: BlockState[][], initial: BlockState) => {
+  for (const row of state) {
     for (const block of row) {
       if (Math.abs(initial.x - block.x) < 1)
         continue
@@ -64,22 +67,8 @@ const generateMines = (initial: BlockState) => {
       block.mine = Math.random() < 0.2
     }
   }
-  updateNumbers()
+  updateNumbers(state)
 }
-
-const numberColors = [
-  'text-transparent',
-  'text-gray-500',
-  'text-blue-500',
-  'text-green-500',
-  'text-yellow-500',
-  'text-red-500',
-  'text-purple-500',
-  'text-pink-500',
-  'text-orange-500',
-  'text-indigo-500',
-  'text-teal-500',
-]
 
 const expandZero = (block: BlockState) => {
   if (block.adjacentMines)
@@ -94,7 +83,6 @@ const expandZero = (block: BlockState) => {
 }
 
 let mineGernerated = false
-const dev = false
 
 const checkGameState = () => {
   if (!mineGernerated)
@@ -108,15 +96,20 @@ const checkGameState = () => {
   }
 }
 
+watchEffect(() => {
+  checkGameState()
+})
+reset()
+
 const onRightClick = (block: BlockState) => {
   if (block.reveoled)
     return
   block.flagged = true
 }
 
-const onClick = (e: MouseEvent, block: BlockState) => {
+const onClick = (block: BlockState) => {
   if (!mineGernerated) {
-    generateMines(block)
+    generateMines(state.value, block)
     mineGernerated = true
   }
   block.reveoled = true
@@ -126,50 +119,27 @@ const onClick = (e: MouseEvent, block: BlockState) => {
   }
   expandZero(block)
 }
-
-const getBlockClass = (block: BlockState) => {
-  if (block.flagged)
-    return 'bg-gray-500/10'
-  if (!block.reveoled)
-    return 'bg-gray-500/10 hover:bg-gray-500/20'
-  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
-}
-
-watchEffect(() => {
-  checkGameState()
-})
 </script>
 
 <template>
   <div>
     <div mb-5>
       Minesweeper / 扫雷
+      <span ml-2>
+        调试:
+        <button ml-2 @click="toggleDev()">
+          {{ isDev }}
+        </button>
+      </span>
     </div>
     <div v-for="row, y in state" :key="y" flex="~" items-center justify-center>
-      <button
+      <MineBlock
         v-for="block, x in row"
         :key="x"
-        w-10 h-10 m="0.5"
-        border="1 gray-400/20"
-        flex="~"
-        items-center
-        justify-center
-        :class="getBlockClass(block)"
-        @click="onClick($event, block)"
+        :block="block"
+        @click="onClick(block)"
         @contextmenu.prevent="onRightClick(block)"
-      >
-        <template v-if="block.flagged">
-          <div i-mdi-flag text-red />
-        </template>
-        <template v-else-if="block.reveoled || dev">
-          <div v-if="block.mine" i-mdi-mine>
-            x
-          </div>
-          <div v-else>
-            {{ block.adjacentMines }}
-          </div>
-        </template>
-      </button>
+      />
     </div>
   </div>
 </template>
