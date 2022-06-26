f6fef0f9ea1fa788c42ca0263f8868e6c6113c4c
guxuerui <im.guxuerui@gmail.com>
2022-06-27 00:52:20 +0800

    add new html

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,7 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AnimateCursor: typeof import('./src/components/AnimateCursor.vue')['default']
     Counter: typeof import('./src/components/Counter.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
     MineBlock: typeof import('./src/components/MineBlock.vue')['default']
---
+++ src/components/AnimateCursor.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <canvas id="canvas" />
+</template>
+
+<style scoped>
+</style>
