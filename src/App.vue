4c2d22cd8fc29f3ecd6f297951ec85ef8d43edeb
guxuerui <im.guxuerui@gmail.com>
2022-06-30 11:48:04 +0800

    fix build error, add toggle cursor animation btn

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+const show = ref<boolean>(false)
 </script>
 
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <router-view />
     <Footer />
-    <AnimateCursor />
+    <AnimateCursor v-if="show" />
+    <button class="icon-btn !outline-none btn-scale" ml-2 @click="show = !show">
+      <div v-if="!show" i-carbon-cursor-2 />
+      <div v-else i-carbon-location-current />
+    </button>
   </main>
 </template>
+
+<style scoped>
+  .btn-scale {
+    position: relative;
+    top: -0.1rem;
+    transform: scale(1.2);
+  }
+</style>
--- src/components/AnimateCursor.vue
+++ src/components/AnimateCursor.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-onMounted(() => {
-  init()
-})
-
+const color = ref<any>(0)
+const color2 = ref<any>(false)
+const round_arr = ref<any>([])
+const context = ref<any>(null)
+const myCanvas = ref<any>(null)
+const WIDTH = ref<number>(0)
+const HEIGHT = ref<number>(0)
 const para = ref({
   num: 100,
   color: false, // 颜色，如果是false则使用随机渐变颜色
@@ -11,53 +14,52 @@ const para = ref({
   a: 1,
 })
 
-const color = ref<string | number>(0)
-const color2 = ref<string | number>(false)
-const round_arr = ref<any>([])
+nextTick(() => {
+  // myCanvas.value = document.getElementById('canvas')!
+  context.value = myCanvas.value.getContext('2d')
+  WIDTH.value = myCanvas.value.width = document.documentElement.clientWidth
+  HEIGHT.value = myCanvas.value.height = document.documentElement.clientHeight
+  animate()
+})
 
-function init() {
-  // const myCanvas = ref<any>(null)
-  const myCanvas = document.getElementById('canvas')
-  const ctx = myCanvas.getContext('2d')
-  const WIDTH = myCanvas.width = document.documentElement.clientWidth
-  const HEIGHT = myCanvas.height = document.documentElement.clientHeight
-  let mouseX = 0
-  let mouseY = 0
+// function init() {
+let mouseX = 0
+let mouseY = 0
 
-  window.onmousemove = function (event) {
-    mouseX = event.clientX
-    mouseY = event.clientY
-    round_arr.value.push({
-      mouseX,
-      mouseY,
-      r: para.value.r,
-      o: 1,
-    })
-  }
-
-  // 判断参数中是否设置了color，如果有，则用该值
-  // 如果参数中的color为false，则使用随机颜色
-  if (para.value.color)
-    color2.value = para.value.color
-  else
-    color.value = Math.random() * 360
-
-  animate(WIDTH, HEIGHT)
+window.onmousemove = function (event) {
+  mouseX = event.clientX
+  mouseY = event.clientY
+  round_arr.value.push({
+    mouseX,
+    mouseY,
+    r: para.value.r,
+    o: 1,
+  })
 }
 
-function animate(width, height) {
+// 判断参数中是否设置了color，如果有，则用该值
+// 如果参数中的color为false，则使用随机颜色
+if (para.value.color)
+  color2.value = para.value.color
+else
+  color.value = Math.random() * 360
+
+// }
+
+function animate() {
   if (!para.value.color) {
     color.value += 0.1
-    color2.value = `hsl(${color.value}, 100%, 80%)`
+    // color2.value = `hsl(${color.value}, 100%, 80%)`
+    color2.value = 'hsl(180, 100%, 80%)'
   }
 
-  ctx.clearRect(0, 0, width, height)
+  context.value.clearRect(0, 0, WIDTH.value, HEIGHT.value)
   for (let i = 0; i < round_arr.value.length; i++) {
-    ctx.fillStyle = color2.value
-    ctx.beginPath()
-    ctx.arc(round_arr.value[i].mouseX, round_arr.value[i].mouseY, round_arr.value[i].r, 0, Math.PI * 2)
-    ctx.closePath()
-    ctx.fill()
+    context.value.fillStyle = color2.value
+    context.value.beginPath()
+    context.value.arc(round_arr.value[i].mouseX, round_arr.value[i].mouseY, round_arr.value[i].r, 0, Math.PI * 2)
+    context.value.closePath()
+    context.value.fill()
     round_arr.value[i].r += para.value.r
     round_arr.value[i].o -= para.value.o
 
@@ -75,4 +77,12 @@ function animate(width, height) {
 </template>
 
 <style scoped>
+  #canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
 </style>
