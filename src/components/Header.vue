ee0477eda9ec2a1424090dbfb5cde1735a5ee620
guxuerui <im.guxuerui@gmail.com>
2022-07-16 23:26:57 +0800

    add file

---
+++ src/components/Header.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+// router link list
+const links = ref([
+  {
+    name: 'Vue',
+    fileName: 'index',
+    folder: 'vue',
+  },
+  {
+    name: 'Js',
+    fileName: 'index',
+    folder: 'js',
+  },
+  {
+    name: 'Css',
+    fileName: 'index',
+    folder: 'css',
+  },
+  {
+    name: 'Note',
+    fileName: 'index',
+    folder: 'note',
+  },
+  {
+    name: 'Minesweeper',
+    fileName: 'sweeper',
+    folder: 'game',
+  },
+])
+// is or not darkMode
+const isDarkMode = computed(() => {
+  return isDark.value
+})
+
+const router = useRouter()
+const jumpPage = (folder: string, name: string) => {
+  if (folder && name)
+    router.push(`/${folder}/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <nav class="header pa-4 link">
+    <div>
+      <a href="/" class="c-gray-500 hover:c-black dark:hover:c-white">
+        Home
+      </a>
+    </div>
+    <div>
+      <a
+        v-for="(item, i) in links"
+        :key="i"
+        class="mr-3 c-gray-500 hover:c-black dark:hover:c-white cursor-pointer"
+        @click="jumpPage(item.folder, item.fileName)"
+      >
+        {{ item.name }}
+      </a>
+      <button class="icon-btn !outline-none vertical-text-top c-gray-800 dark:c-gray-400 hover:c-black dark:hover:c-white" @click="toggleDark()">
+        <div v-if="isDarkMode" i-carbon-moon />
+        <div v-else i-carbon-sun />
+      </button>
+      <a
+        class="vertical-text-top ml-3 c-gray-400 hover:c-black dark:hover:c-white"
+        rel="noreferrer"
+        href="https://github.com/guxuerui/vue3-minesweeper"
+        target="_blank"
+        title="GitHub"
+      >
+        <span class="inline-block" i-carbon-logo-github />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .link div a {
+    transition: color 0.2s ease-in-out;
+  }
+</style>
---
+++ src/pages/game/sweeper.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { isDev, toggleDev } from '~/composables'
+const play = new GamePlay(10, 10)
+const state = play.state
+
+watchEffect(() => {
+  play.checkGameState()
+})
+
+const router = useRouter()
+const jumpPage = (folder: string, name: string) => {
+  if (folder && name)
+    router.push(`/${folder}/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div mb-5>
+      Minesweeper / 扫雷
+    </div>
+    <div mb-4>
+      <button btn @click="jumpPage('blog', 'test4')">
+        TO MD PAGE
+      </button>
+    </div>
+    <div v-for="row, y in state" :key="y" flex="~" items-center justify-center>
+      <MineBlock
+        v-for="block, x in row"
+        :key="x"
+        :block="block"
+        @click="play.onClick(block)"
+        @contextmenu.prevent="play.onRightClick(block)"
+      />
+    </div>
+    <div flex="~ gap-2" mt-4 justify-center>
+      <button btn @click="toggleDev()">
+        {{ isDev ? '开发模式' : '游戏模式' }}
+      </button>
+      <button btn @click="play.reset()">
+        重新开始
+      </button>
+    </div>
+  </div>
+</template>
