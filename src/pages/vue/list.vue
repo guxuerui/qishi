c7de61fc6ed4e67b90e0d72e753501d5072f0c38
guxuerui <im.guxuerui@gmail.com>
2022-07-17 23:47:09 +0800

    feat(vue): new vue folder

---
+++ src/pages/vue/list.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const postList = ref([
+  {
+    title: 'Vue3之呼吸效果状态组件',
+    date: '2022-02-18',
+    folder: 'vue',
+    fileName: 'post1',
+  },
+  {
+    title: '二次封装BalmUI表格组件',
+    date: '2022-02-17',
+    folder: 'vue',
+    fileName: 'post2',
+  },
+])
+</script>
+
+<template>
+  <ul class="list-title">
+    <li v-for="(item, i) in postList" :key="i" class="w-full flex justify-between mb-4">
+      <a class="cursor-pointer c-gray-600 hover:c-black dark:c-gray-400 dark:hover:c-white">
+        {{ item.title }}
+      </a>
+      <div class="sub-title flex items-center c-gray-600 dark:c-gray-400">
+        {{ item.date }}
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  .sub-title {
+    font-size: 0.8rem;
+  }
+</style>
