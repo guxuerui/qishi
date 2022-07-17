229799a55d63a45356c8c8397fb310992f8fc3ba
guxuerui <im.guxuerui@gmail.com>
2022-07-17 23:55:47 +0800

    feat(list.vue): add jump fn

--- src/pages/vue/list.vue
+++ src/pages/vue/list.vue
@@ -4,21 +4,30 @@ const postList = ref([
     title: 'Vue3之呼吸效果状态组件',
     date: '2022-02-18',
     folder: 'vue',
-    fileName: 'post1',
+    fileName: 'breathLightComp',
   },
   {
     title: '二次封装BalmUI表格组件',
     date: '2022-02-17',
     folder: 'vue',
-    fileName: 'post2',
+    fileName: 'refactorBalmDataTable',
   },
 ])
+
+const router = useRouter()
+const jumpPage = (folder: string, name: string) => {
+  if (folder && name)
+    router.push(`/${folder}/${encodeURIComponent(name)}`)
+}
 </script>
 
 <template>
   <ul class="list-title">
     <li v-for="(item, i) in postList" :key="i" class="w-full flex justify-between mb-4">
-      <a class="cursor-pointer c-gray-600 hover:c-black dark:c-gray-400 dark:hover:c-white">
+      <a
+        class="cursor-pointer c-gray-600 hover:c-black dark:c-gray-400 dark:hover:c-white"
+        @click="jumpPage(item.folder, item.fileName)"
+      >
         {{ item.title }}
       </a>
       <div class="sub-title flex items-center c-gray-600 dark:c-gray-400">
