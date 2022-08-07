2105004ee9d6faa5edc979b2d9f605e46a4afbfc
guxuerui <im.guxuerui@gmail.com>
2022-08-07 19:54:11 +0800

    feat: draw tree

--- src/pages/game/drawTree.vue
+++ src/pages/game/drawTree.vue
@@ -1,10 +1,105 @@
 <script setup lang="ts">
+const el = $ref<HTMLCanvasElement>()
+const ctx = $computed(() => el!.getContext('2d')!)
+const img = new Image()
+img.src = '/imgs/lotus_flower.svg'
+
+const WIDTH = 800
+const HEIGHT = 800
+
+interface Point {
+  x: number
+  y: number
+}
+
+interface Branch {
+  start: Point
+  length: number
+  theta: number
+}
+
+function init() {
+  ctx.strokeStyle = `rgb(255, ${Math.floor(255 - 40 * Math.random() * 6)}, ${Math.floor(255 - 40 * Math.random() * 6)})`
+  // ctx.strokeStyle = '#6b7280'
+  step({
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 50,
+    theta: -Math.PI / 2,
+  })
+}
+
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  }
+}
+
+const pendingTasks: Function[] = []
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length * (0.7 + 0.5 * Math.random()),
+      theta: b.theta - 0.3 * Math.random(),
+    }, depth + 1))
+  }
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length * (0.7 + 0.3 * Math.random()),
+      theta: b.theta + 0.3 * Math.random(),
+    }, depth + 1))
+  }
+}
+
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath()
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
+  ctx.lineCap = 'round'
+  ctx.stroke()
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b))
+  // drawImg(img, getEndPoint(b))
+}
+
+function drawImg(img: HTMLImageElement, p: Point) {
+  ctx.drawImage(img, p.x - 5, p.y - 5, 20, 20)
+}
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let framesCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    framesCount += 1
+    if (framesCount % 3 === 0)
+      frame()
+
+    startFrame()
+  })
+}
+startFrame()
+
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
-  <div>test</div>
+  <div class="markdown-body">
+    <h1>用canvas画一棵树</h1>
+  </div>
+  <canvas ref="el" width="800" height="800" border />
 </template>
 
-<style scoped>
-
-</style>
