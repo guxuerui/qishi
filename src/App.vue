22b6123c1e251707c5b15102f53036fb35c4e808
guxuerui <im.guxuerui@gmail.com>
2022-08-18 18:10:42 +0800

    fix: delete comment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-// const show = ref<boolean>(false)
 const routes = useRoute()
 </script>
 
@@ -8,11 +7,6 @@ const routes = useRoute()
   <main class="main" font-sans p="x-4 y-5" text="center gray-700 dark:gray-200">
     <router-view />
     <Footer v-if="routes.fullPath !== '/'" />
-    <!-- <AnimateCursor v-if="show" /> -->
-    <!-- <button class="icon-btn !outline-none btn-scale" ml-2 @click="show = !show"> -->
-    <!--   <div v-if="!show" i-carbon-cursor-2 /> -->
-    <!--   <div v-else i-carbon-location-current /> -->
-    <!-- </button> -->
   </main>
 </template>
 
@@ -22,10 +16,5 @@ const routes = useRoute()
     text-align: left;
     margin: 0 auto;
     font-size: 1rem;
-  }
-  .btn-scale {
-    position: relative;
-    top: -0.1rem;
-    transform: scale(1.2);
   }
 </style>
