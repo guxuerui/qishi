42178c9d14a2e8c8b41f10d5a4304702e10c0087
guxuerui <im.guxuerui@gmail.com>
2022-12-07 19:29:24 +0800

    feat(types.ts): new interface

--- src/types.ts
+++ src/types.ts
@@ -46,3 +46,23 @@ export interface BlockArea {
   rotate: BlockRotate
   areas: Area[]
 }
+
+export interface IList {
+  date: string
+  iconColor?: string
+  iconRight?: string
+  iconTop?: string
+  chipColor?: string
+  chipText?: string
+  subTitleColor?: string
+  subTitle: string
+  imgUrl?: string
+  content: string
+}
+
+export interface ITimeline {
+  isCollapsed: boolean
+  year: string
+  title: string
+  list: IList[]
+}
