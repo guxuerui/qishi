4f224a325faefe275b3c5cc3c4bccf6caaa617d3
guxuerui <im.guxuerui@gmail.com>
2023-03-27 17:51:29 +0800

    ✨ feat(ProgressBar.vue): add ProgressBar component with progress prop and styling

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps({
+  progress: {
+    type: String,
+    required: true,
+    default: '25%',
+  },
+})
+</script>
+
+<template>
+   <div class="shell">
+    <div class="bar" :style="{ width: progress }">
+      <span>{{ progress }}%</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.shell {
+  height: 30px;
+  width: 250px;
+  border: 1px solid #aaa;
+  border-radius: 13px;
+  padding: 4px;
+}
+
+.bar {
+  background: linear-gradient(to right, #B993D6, #8CA6DB);
+  height: 20px;
+  border-radius: 10px;
+  animation: width 1s ease-in-out;
+}
+
+.bar span {
+  float: right;
+  color: #fff;
+  font-size: 0.8rem;
+  padding-right: 8px;
+}
+
+@keyframes width {
+  from {
+    width: 0%;
+  }
+  to {
+    width: v-bind(progress);
+  }
+}
+</style>
