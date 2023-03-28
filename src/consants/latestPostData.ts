643f7daec2203270527aac313d34181894281363
guxuerui <im.guxuerui@gmail.com>
2023-03-28 21:50:48 +0800

    refactor(latestPostData.ts): refactor function logic

--- src/consants/latestPostData.ts
+++ src/consants/latestPostData.ts
@@ -1,10 +1,12 @@
 export const getLatestPost = async () => {
-  const latestVue = await getMetaData(`${vuePostList.value[0].folder}`, `${vuePostList.value[0].fileName}.md`)
-  const latestJs = await getMetaData(`${jsPostList.value[0].folder}`, `${jsPostList.value[0].fileName}.md`)
-  const latestCss = await getMetaData(`${cssPostList.value[0].folder}`, `${cssPostList.value[0].fileName}.md`)
-  const latestNote = await getMetaData(`${notePostList.value[0].folder}`, `${notePostList.value[0].fileName}.md`)
-  const latestGame = await getMetaData(`${gamePostList.value[0].folder}`, `${gamePostList.value[0].fileName}.vue`)
+  const latestPostList = await Promise.all([
+    getMetaData(`${vuePostList.value[0].folder}`, `${vuePostList.value[0].fileName}.md`),
+    getMetaData(`${vuePostList.value[1].folder}`, `${vuePostList.value[1].fileName}.md`),
+    getMetaData(`${jsPostList.value[0].folder}`, `${jsPostList.value[0].fileName}.md`),
+    getMetaData(`${cssPostList.value[0].folder}`, `${cssPostList.value[0].fileName}.md`),
+    getMetaData(`${notePostList.value[0].folder}`, `${notePostList.value[0].fileName}.md`),
+    getMetaData(`${gamePostList.value[0].folder}`, `${gamePostList.value[0].fileName}.vue`),
+  ])
 
-  const latestPostList = [latestVue, latestJs, latestCss, latestNote, latestGame]
   return latestPostList
 }
