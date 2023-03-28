84cdf92eaa0b6b420b0cad836a83ca81877990c0
guxuerui <im.guxuerui@gmail.com>
2023-03-28 21:51:45 +0800

    feat(index.vue): refactor index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,12 +3,7 @@ import { ChipsData, SkillListData } from '~/consants/skillData'
 import { getLatestPost } from '~/consants/latestPostData'
 import type { IPostCard } from '~/types'
 
-const router = useRouter()
 const latestPostData = ref<IPostCard[]>([])
-const jumpPage = (folder: string, name: string) => {
-  if (folder && name)
-    router.push(`/${folder}/${encodeURIComponent(name)}`)
-}
 
 onMounted(async () => {
   latestPostData.value = await getLatestPost()
@@ -21,14 +16,12 @@ onMounted(async () => {
     <div class="text-linear" flex="~ gap-x-2">
       <img v-for="(chip, i) in ChipsData" :key="i" my-0 :src="chip" alt="" class="!my-0">
     </div>
-    <p class="text-linear">
-      欢迎来到我的个人空间，在这里记录我的学习过程，希望能够帮助到更多的朋友~
-    </p>
-    <p class="text-linear">
-      我喜欢做一些有意思的小项目
+    <p class="text-linear !my-3">
+      前端开发 / 业余LOL玩家<br>
+      <span text-sm>我喜欢做一些有意思的小项目</span>
     </p>
     <a
-      class="c-gray-400"
+      class="c-gray-400 w-20"
       flex="~ gap-x-1" items-center
       hover="c-black"
       dark:hover="c-white"
@@ -40,6 +33,16 @@ onMounted(async () => {
       <span class="inline-block" i-carbon-logo-github />
       <span>github</span>
     </a>
+    <h3>Used Language Rank -></h3>
+    <div grid="~ cols-1">
+      <ProgressBar
+        v-for="(bar, i) in SkillListData"
+        :key="i"
+        :label="bar.label"
+        :progress="bar.progress"
+        :bar-color="bar.color"
+      />
+    </div>
     <h3>Latest Posts</h3>
     <div grid="~ cols-1 gap-6" class="sm:grid-cols-2">
       <PostCard
@@ -50,16 +53,7 @@ onMounted(async () => {
         :date="card.date"
         :chip-text="card.chipText"
         :chip-color="card.chipColor"
-      />
-    </div>
-    <h3>Used Language Rank -></h3>
-    <div grid="~ cols-1">
-      <ProgressBar
-        v-for="(bar, i) in SkillListData"
-        :key="i"
-        :label="bar.label"
-        :progress="bar.progress"
-        :bar-color="bar.color"
+        :file-name="card.fileName"
       />
     </div>
   </div>
