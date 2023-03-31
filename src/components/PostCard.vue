5363ed5e59030f51cb8f23bb9d6c1bba7770c2d0
guxuerui <im.guxuerui@gmail.com>
2023-03-31 11:40:05 +0800

    🎨 style(PostCard.vue): add border animation on hover 🐛 fix(PostCard.vue): fix position of wrapper element to relative

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -31,6 +31,8 @@ defineProps({
     default: '',
   },
 })
+// 边框宽度
+const borderWidth = ref('2px')
 
 const router = useRouter()
 const jumpPage = (folder: string, name: string) => {
@@ -40,7 +42,7 @@ const jumpPage = (folder: string, name: string) => {
 </script>
 
 <template>
-  <div class="max-w-xl transition px-6 py-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:border-gray-700 dark:hover:border-gray-200">
+  <div class="wrapper max-w-xl transition px-6 py-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 position-relative">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white cursor-pointer">
       {{ title }}
     </h5>
@@ -68,3 +70,39 @@ const jumpPage = (folder: string, name: string) => {
     </div>
   </div>
 </template>
+
+<style scoped>
+  .wrapper:before, .wrapper:after {
+    content: '';
+    width: 0%;
+    height: 0%;
+    position: absolute;
+    visibility: hidden;
+    border-radius: .5rem;
+  }
+  .wrapper:before {
+    border-top: v-bind(borderWidth) solid #01ffc2;
+    border-right: v-bind(borderWidth) solid #01ffc2;
+    transition: width .1s ease .1s * 3, height .1s ease .1s * 2, visibility 0s .1s * 4;
+    top: 0;
+    left: 0;
+  }
+  .wrapper:after {
+    border-left: v-bind(borderWidth) solid #01a4ff;
+    border-bottom: v-bind(borderWidth) solid #01a4ff;
+    bottom: 0;
+    right: 0;
+    transition: width .1s ease .1s, height .1s ease, visibility 0s .1s * 2;
+  }
+  .wrapper:hover:before, .wrapper:hover:after {
+    width: 100%;
+    height: 100%;
+    visibility: visible;
+  }
+  .wrapper:hover:before {
+    transition: width .1s ease, height .1s ease .1s;
+  }
+  .wrapper:hover:after {
+    transition: width .1s ease .1s * 2, height .1s ease .1s * 3, visibility 0s .1s * 2;
+  }
+</style>
