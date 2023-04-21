dbe3372df14bc12223858c74eb3562fa866ad660
guxuerui <im.guxuerui@gmail.com>
2023-04-21 16:30:41 +0800

    🔨 refactor(latestPostData.ts): add return type to getLatestPost function and use file type property instead of hardcoding file extension

--- src/consants/latestPostData.ts
+++ src/consants/latestPostData.ts
@@ -1,11 +1,13 @@
-export const getLatestPost = async () => {
+import type { IPostCard } from '~/types'
+
+export const getLatestPost = async (): Promise<IPostCard[]> => {
   const latestPostList = await Promise.all([
-    getMetaData(`${vuePostList.value[0].folder}`, `${vuePostList.value[0].fileName}.md`),
-    getMetaData(`${vuePostList.value[1].folder}`, `${vuePostList.value[1].fileName}.md`),
-    getMetaData(`${jsPostList.value[0].folder}`, `${jsPostList.value[0].fileName}.md`),
-    getMetaData(`${cssPostList.value[0].folder}`, `${cssPostList.value[0].fileName}.md`),
-    getMetaData(`${notePostList.value[0].folder}`, `${notePostList.value[0].fileName}.md`),
-    getMetaData(`${gamePostList.value[0].folder}`, `${gamePostList.value[0].fileName}.vue`),
+    getMetaData(`${vuePostList.value[0].folder}`, `${vuePostList.value[0].fileName}.${vuePostList.value[0].fileType}`),
+    getMetaData(`${vuePostList.value[1].folder}`, `${vuePostList.value[1].fileName}.${vuePostList.value[1].fileType}`),
+    getMetaData(`${jsPostList.value[0].folder}`, `${jsPostList.value[0].fileName}.${jsPostList.value[0].fileType}`),
+    getMetaData(`${cssPostList.value[0].folder}`, `${cssPostList.value[0].fileName}.${cssPostList.value[0].fileType}`),
+    getMetaData(`${notePostList.value[0].folder}`, `${notePostList.value[0].fileName}.${notePostList.value[0].fileType}`),
+    getMetaData(`${gamePostList.value[0].folder}`, `${gamePostList.value[0].fileName}.${gamePostList.value[0].fileType}`),
   ])
 
   return latestPostList
