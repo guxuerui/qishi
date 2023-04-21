6558ba9b0d04c7822ddecd99c659cbe7a2cb8b35
guxuerui <im.guxuerui@gmail.com>
2023-04-21 16:29:41 +0800

    📝 chore(composables): add fileType property to Post objects

--- src/composables/jsPost.ts
+++ src/composables/jsPost.ts
@@ -6,17 +6,20 @@ export const jsPostList = ref<Post[]>([
     date: '2022-09-07',
     folder: 'js',
     fileName: 'canvasExample',
+    fileType: 'md',
   },
   {
     title: '序列生成器range方法',
     date: '2022-09-01',
     folder: 'js',
     fileName: 'range',
+    fileType: 'md',
   },
   {
     title: '找出数组中只出现过一次的数',
     date: '2022-07-25',
     folder: 'js',
     fileName: 'singleNumber',
+    fileType: 'md',
   },
 ])
