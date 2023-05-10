59e8ac6b110eedfd4409d33728e9745f77ccd2eb
guxuerui <im.guxuerui@gmail.com>
2023-05-10 21:32:56 +0800

    🎨 style(flowerTree.vue): change page title to Chinese ✨ feat(flowerTree.vue): add canvas element to display a tree with flowers

--- src/pages/game/flowerTree.vue
+++ src/pages/game/flowerTree.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { useTitle } from '@vueuse/core'
+
+const title = useTitle()
+
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el!.getContext('2d')!)
 const img = new Image()
 img.src = '/imgs/lotus_flower.svg'
 
 onMounted(() => {
+  title.value = '开花的树'
+
   const Tree = new DrawTree(800, 800, 0, ctx, img)
   // start requestAnimationFrame
   Tree.startFrame()
@@ -12,8 +18,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="markdown-body">
-    <h1>会开花的树</h1>
+  <div class="markdown-body mb-4">
+    <h1>开花的树</h1>
+    <canvas ref="el" width="800" height="800" border />
   </div>
-  <canvas ref="el" width="800" height="800" border />
 </template>
