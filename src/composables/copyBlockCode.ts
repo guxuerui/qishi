36ab9fc535a5a8fe8d3921485431199ebf193704
guxuerui <im.guxuerui@gmail.com>
2023-05-24 20:36:58 +0530

    🎨 style(markdown.css): adjust copy button position to bottom: 0 ✨ feat(copyBlockCode.ts): add SVG icons for copy and copied states

--- src/composables/copyBlockCode.ts
+++ src/composables/copyBlockCode.ts
@@ -1,8 +1,12 @@
 import { copyToClipboard } from 'utils-snap-fn'
 
-const copyIcon = '<div class="dark:text-white" i-carbon-copy />'
+const copyIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32"><path fill="currentColor" d="M28 10v18H10V10h18m0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2Z"/><path fill="currentColor" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z"/></svg>
+`
 
-const copiedIcon = '<div class="dark:text-white" i-carbon-checkmark-outline />'
+const copiedIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32"><path fill="currentColor" d="m14 21.414l-5-5.001L10.413 15L14 18.586L21.585 11L23 12.415l-9 8.999z"/><path fill="currentColor" d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z"/></svg>
+`
 
 function htmlDecode(input: any) {
   const doc = new DOMParser().parseFromString(input, 'text/html')
