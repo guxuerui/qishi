5669cbadd93662812cbd517c8630537bf08b5f35
guxuerui <im.guxuerui@gmail.com>
2023-05-24 18:40:41 +0800

    🎨 style(App.vue, markdown.css): add copy button to code blocks in markdown and style it

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 const routes = useRoute()
+
+function setCopy() {
+  if (!navigator.clipboard)
+    return
+
+  const preTags = document.getElementsByTagName('pre')
+  setTimeout(() => {
+    const preTagsArr = Array.from(preTags)
+
+    for (let i = 0; i < preTagsArr.length; i++) {
+      preTagsArr[i].style.position = 'relative'
+      const copyBtn = document.createElement('button')
+      copyBtn.innerHTML = '<span i-carbon-copy>copy</span>'
+      copyBtn.classList.add('copy-btn')
+      preTagsArr[i].appendChild(copyBtn)
+      copyBtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(preTagsArr[i].innerHTML.trim())
+          .then(() => alert('Code Copied!'))
+          .catch(err => console.error('Failed to cpoy code: ', err))
+      })
+    }
+  }, 100)
+}
+
+watch(() => routes.fullPath, setCopy, { immediate: true })
 </script>
 
 <template>
