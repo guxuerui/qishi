008b198e7cf15b5a712794c2200a94d6f91b3f4c
guxuerui <im.guxuerui@gmail.com>
2023-06-12 22:36:45 +0800

    🎨 style(CircleProgressBar.vue): add props for customizing colors and refactor useTranslate function to use switch statement

--- src/components/CircleProgressBar.vue
+++ src/components/CircleProgressBar.vue
@@ -8,6 +8,22 @@ const props = defineProps({
     type: String,
     default: '0%',
   },
+  startColor: {
+    type: String,
+    default: '#25AABE',
+  },
+  endColor: {
+    type: String,
+    default: '#C2FE89',
+  },
+  valueColor: {
+    type: String,
+    default: '#80BD68',
+  },
+  titleColor: {
+    type: String,
+    default: '#80BD8f',
+  },
 })
 
 // 圆环进度
@@ -16,12 +32,16 @@ const strokeValue = computed(() => {
 })
 
 function useTranslate() {
-  if (props.percentValue.length >= 4)
-    return 'translate(45 84)'
-  else if (props.percentValue.length === 3)
-    return 'translate(58 84)'
-  else
-    return 'translate(70 84)'
+  switch (props.percentValue.length) {
+    case 5:
+      return 'translate(35 84)'
+    case 4:
+      return 'translate(48 84)'
+    case 3:
+      return 'translate(58 84)'
+    default:
+      return 'translate(70 84)'
+  }
 }
 </script>
 
@@ -29,8 +49,8 @@ function useTranslate() {
   <svg width="195" height="195" class="scale-75" xmlns="http://www.w3.org/2000/svg">
     <defs>
       <linearGradient id="grident" x1="0%" y1="0%" x2="102%" y2="101%">
-        <stop stop-color="#25AABE" offset="0%" />
-        <stop stop-color="#C2FE89" offset="100%" />
+        <stop :stop-color="startColor" offset="0%" />
+        <stop :stop-color="endColor" offset="100%" />
       </linearGradient>
     </defs>
     <g fill="none" fill-rule="evenodd">
@@ -38,10 +58,10 @@ function useTranslate() {
         <circle opacity=".5" cx="86.5" cy="86.5" r="86.5" />
         <path d="M86.5 0C134.273 0 173 38.727 173 86.5S134.273 173 86.5 173 0 134.273 0 86.5 38.727 0 86.5 0z" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="226,301,226,299" class="progress-bar" />
       </g>
-      <text font-size="40" font-weight="300" fill="#80BD68" :transform="useTranslate()">
+      <text font-size="40" font-weight="300" :fill="valueColor" :transform="useTranslate()">
         <tspan x="0" y="20">{{ percentValue }}</tspan>
       </text>
-      <text font-size="20" font-weight="300" fill="#eee" transform="translate(68 104)">
+      <text font-size="20" font-weight="300" :fill="titleColor" transform="translate(68 104)">
         <tspan x="0" y="30">{{ title }}</tspan>
       </text>
     </g>
