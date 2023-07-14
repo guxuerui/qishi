a02f24adae6eb405f81ef159adec65a876fc3d78
guxuerui <im.guxuerui@gmail.com>
2023-07-15 00:04:21 +0800

    🔧 chore(CssFilter.vue): refactor code to use reactive data for slide values 🔧 chore(CssFilter.vue): update class names for image elements 🔧 chore(CssFilter.vue): update filter values in style section

--- src/components/CssFilter.vue
+++ src/components/CssFilter.vue
@@ -1,193 +1,91 @@
 <script setup lang="ts">
+import { FilterCards } from '~/constants/FilterCards'
+import type { SearchIndex } from '~/types'
 import image from '/imgs/street.jpeg'
 
-const slider1 = $ref(0)
-const slider1Val = computed(() => `${slider1}%`)
+const slidesData = reactive<SearchIndex>({
+  slide1: 0,
+  slide2: 0,
+  slide3: 1,
+  slide4: 100,
+  slide5: 0,
+  slide6: 100,
+  slide7: 100,
+  slide8: 100,
+  slide9: 5,
+  slide10: 8,
+})
 
-const slider2 = $ref(0)
-const slider2Val = computed(() => `${slider2}%`)
+// const slide1 = $ref(0)
+const slide1Val = computed(() => `${slidesData.slide1}%`)
 
-const slider3 = $ref(1)
-const slider3Val = computed(() => `${slider3}%`)
+const slide2Val = computed(() => `${slidesData.slide2}%`)
 
-const slider4 = $ref(100)
-const slider4Val = computed(() => `${slider4}deg`)
+const slide3Val = computed(() => `${slidesData.slide3}%`)
 
-const slider5 = $ref(0)
-const slider5Val = computed(() => `${slider5}%`)
+const slide4Val = computed(() => `${slidesData.slide4}deg`)
 
-const slider6 = $ref(100)
-const slider6Val = computed(() => `${slider6}%`)
+const slide5Val = computed(() => `${slidesData.slide5}%`)
 
-const slider7 = $ref(100)
-const slider7Val = computed(() => `${slider7}%`)
+const slide6Val = computed(() => `${slidesData.slide6}%`)
 
-const slider8 = $ref(100)
-const slider8Val = computed(() => `${slider8}%`)
+const slide7Val = computed(() => `${slidesData.slide7}%`)
 
-const slider9 = $ref(5)
-const slider9Val = computed(() => `${slider9}px`)
+const slide8Val = computed(() => `${slidesData.slide8}%`)
 
-const slider10 = $ref(8)
-const slider10Val = computed(() => `${slider10}px`)
+const slide9Val = computed(() => `${slidesData.slide9}px`)
+
+const slide10Val = computed(() => `${slidesData.slide10}px`)
 </script>
 
 <template>
-  <div flex="~ gap-6">
-    <div min-h-50 w-85>
+  <div grid="~ gap-6 md:cols-2 sm:cols-1">
+    <div v-for="item, i in FilterCards" :key="i" min-h-50>
       <div h-20 overflow-scroll>
-        1. grayscale() 函数将改变图像灰度值。取值在0%-100%之间，默认为0
+        {{ i + 1 }}. {{ item.title }}
       </div>
       <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider1 }}</div>
-        <div>Max: 100</div>
+        <div>Min: {{ item.min }}</div>
+        <div>current: {{ slidesData[`slide${i + 1}`] }}</div>
+        <div v-if="item.max">
+          Max: {{ item.max }}
+        </div>
       </div>
-      <input v-model="slider1" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider1">
-    </div>
-    <div min-h-50 w-85>
-      <div h-20 overflow-scroll>
-        2. sepia() 函数将图像变为深褐色。取值在0%-100%之间，默认为0
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider2 }}</div>
-        <div>Max: 100</div>
-      </div>
-      <input v-model="slider2" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider2">
-    </div>
-  </div>
-  <div flex="~ gap-6">
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        3. saturate() 函数转换图像饱和度。取值应大于等于0，默认为1
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider3 }}</div>
-      </div>
-      <input v-model="slider3" type="range" min="0" max="10000" step="1">
-      <img :src="image" class="slider3">
-    </div>
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        4. hue-rotate() 函数对图像做色相旋转。默认为0deg，0-360deg为一个循环，超过360deg相当于又从0开始
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider4 }}</div>
-      </div>
-      <input v-model="slider4" type="range" min="0" max="10000" step="1">
-      <img :src="image" class="slider4">
-    </div>
-  </div>
-  <div flex="~ gap-6">
-    <div min-h-50 w-85>
-      <div h-20 overflow-scroll>
-        5. invert() 函数反转图像。取值为0%-100%，默认为0%
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider5 }}</div>
-        <div>Max: 100</div>
-      </div>
-      <input v-model="slider5" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider5">
-    </div>
-    <div min-h-50 w-85>
-      <div h-20 overflow-scroll>
-        6. opacity() 函数转化图像的透明度。取值在0%-100%之间，默认为100%
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider6 }}</div>
-        <div>Max: 100</div>
-      </div>
-      <input v-model="slider6" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider6">
-    </div>
-  </div>
-  <div flex="~ gap-6">
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        7. brightness() 函数调整图像明亮度。可以为数字或百分比值，默认为1即100%
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider7 }}</div>
-      </div>
-      <input v-model="slider7" type="range" min="0" max="3000" step="1">
-      <img :src="image" class="slider7">
-    </div>
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        8. contrast() 函数调整图像的对比度。值大于等于0%，默认为100%
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider8 }}</div>
-      </div>
-      <input v-model="slider8" type="range" min="0" max="3000" step="1">
-      <img :src="image" class="slider8">
-    </div>
-  </div>
-  <div flex="~ gap-6">
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        9. blur() 函数将高斯模糊应用于图像。默认为0，不能是百分比值
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider9 }}px</div>
-      </div>
-      <input v-model="slider9" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider9">
-    </div>
-    <div min-h-50 w-85>
-      <div h-24 overflow-scroll>
-        10. drop-shadow() 函数设置图像阴影效果。使用drop-shadow(offset-x offset-y blur-raduis? color?)
-      </div>
-      <div flex="~" justify-between>
-        <div>Min: 0</div>
-        <div>current: {{ slider10 }}</div>
-      </div>
-      <input v-model="slider10" type="range" min="0" max="100" step="1">
-      <img :src="image" class="slider10">
+      <input v-model="slidesData[`slide${i + 1}`]" type="range" min="0" max="100" step="1">
+      <img :src="image" :class="item.className">
     </div>
   </div>
 </template>
 
 <style scoped>
-  .slider1 {
-    filter: grayscale(v-bind(slider1Val));
+  .slide1 {
+    filter: grayscale(v-bind(slide1Val));
   }
-  .slider2 {
-    filter: sepia(v-bind(slider2Val));
+  .slide2 {
+    filter: sepia(v-bind(slide2Val));
   }
-  .slider3 {
-    filter: saturate(v-bind(slider3Val));
+  .slide3 {
+    filter: saturate(v-bind(slide3Val));
   }
-  .slider4 {
-    filter: hue-rotate(v-bind(slider4Val));
+  .slide4 {
+    filter: hue-rotate(v-bind(slide4Val));
   }
-  .slider5 {
-    filter: invert(v-bind(slider5Val));
+  .slide5 {
+    filter: invert(v-bind(slide5Val));
   }
-  .slider6 {
-    filter: opacity(v-bind(slider6Val));
+  .slide6 {
+    filter: opacity(v-bind(slide6Val));
   }
-  .slider7 {
-    filter: brightness(v-bind(slider7Val));
+  .slide7 {
+    filter: brightness(v-bind(slide7Val));
   }
-  .slider8 {
-    filter: contrast(v-bind(slider8Val));
+  .slide8 {
+    filter: contrast(v-bind(slide8Val));
   }
-  .slider9 {
-    filter: blur(v-bind(slider9Val));
+  .slide9 {
+    filter: blur(v-bind(slide9Val));
   }
-  .slider10 {
-    filter: drop-shadow(8px 10px v-bind(slider10Val) #585);
+  .slide10 {
+    filter: drop-shadow(8px 10px v-bind(slide10Val) #585);
   }
 </style>
