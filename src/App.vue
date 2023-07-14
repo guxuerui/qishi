7836e0fafc4bc5c81772343766fb21bc7271e05d
guxuerui <im.guxuerui@gmail.com>
2023-07-14 12:28:43 +0800

    🎨 style(App.vue): update main container styles
    - Increase the maximum width of the main container to 5xl on medium screens and above.
    - Center the main container horizontally.
    - Change the text alignment to left.
    - Update the font color to gray-700 in light mode and gray-200 in dark mode.
    
    The scoped CSS style for the main container has been removed.

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,16 @@ watch(() => routes.fullPath, (newVal, _oldVal) => {
 
 <template>
   <Header />
-  <main class="main slide-enter-content" font-sans p="x-4 y-5" text="center gray-700 dark:gray-200">
+  <main
+    class="slide-enter-content md:max-w-5xl"
+    font-sans
+    m="x-auto y-0"
+    p="x-4 y-5"
+    text="left gray-700 dark:gray-200"
+  >
     <router-view class="slide-enter-content" />
     <Footer v-if="routes.fullPath !== '/' && !routes.fullPath.includes('list')" />
   </main>
   <BackTop />
   <PageBg />
 </template>
-
-<style scoped>
-  .main {
-    max-width: 70ch;
-    text-align: left;
-    margin: 0 auto;
-    font-size: 1rem;
-  }
-</style>
