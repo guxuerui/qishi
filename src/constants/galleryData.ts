5892f98e4be91ac8c05da25126ea303606090dcf
guxuerui <im.guxuerui@gmail.com>
2023-07-20 15:27:16 +0800

    📝 chore(galleryData.ts): add new gallery item with title, date, color, and author

--- src/constants/galleryData.ts
+++ src/constants/galleryData.ts
@@ -8,6 +8,12 @@ interface GallaryItems extends Pick<Post, 'title' | 'date'> {
 
 export const galleryItems = ref<GallaryItems[]>([
   {
+    title: '机会就是在时间之流中，最好的一刹那。',
+    date: '2023-07-20',
+    color: randomColor('rgb', 0.3),
+    author: '佚名',
+  },
+  {
     title: '囤尖省，日子长；囤底省，打饥荒。',
     date: '2023-07-19',
     color: randomColor('rgb', 0.3),
