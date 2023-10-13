b0ab4a4dbdf4129314325fc68816d5a41efbab4d
guxuerui <im.guxuerui@gmail.com>
2023-10-13 18:49:39 +0800

    refactor: Rename project card title and project URL

--- src/constants/projectCardsData.ts
+++ src/constants/projectCardsData.ts
@@ -3,11 +3,11 @@ import type { IProjectCard } from '~/types'
 export default function getProjectCardsData(): IProjectCard[] {
   return [
     {
-      title: 'nvim-0.8',
+      title: 'nvim-config',
       borderColor: '#5EADEB',
       abstract: 'Neovim是基于`vim`的现代文本编辑器, 它旨在改进`vim`的可扩展性和用户友好性, 这是我的Neovim配置文件.',
       imgUrl: '/imgs/nvim.png',
-      projectUrl: 'https://github.com/guxuerui/nvim-0.8',
+      projectUrl: 'https://github.com/guxuerui/nvim-config',
     },
     {
       title: 'emoji-picker',
